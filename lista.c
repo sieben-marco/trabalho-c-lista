7573078ebfccd809405ff8d5847392f474ad9d8a
Larissa Dias <154644799+larissa-dias3@users.noreply.github.com>
2024-10-13 15:52:06 -0300

    inicializacao de lista e criacao de .gitignore

--- lista.c
+++ lista.c
@@ -1,1 +1,12 @@
 // desenvolver lista
+#include<stdio.h>
+int main(){
+
+    int vet[9];
+
+    for(int i = 0; i <=9; i++){
+        printf("\nInforme qual a posicao do codigo desejado:");
+        scanf("%d", vet[i]);
+    }
+    return 0;
+}
