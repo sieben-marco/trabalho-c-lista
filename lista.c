9193becb9fe47d48289a9717f1cfe3243f516425
Larissa Dias <154644799+larissa-dias3@users.noreply.github.com>
2024-10-13 16:26:21 -0300

    Criacao de funcao para mostrar menu

--- lista.c
+++ lista.c
@@ -1,12 +1,22 @@
 // desenvolver lista
 #include<stdio.h>
+int vet[9];
+//assinatura da funcao indica que sera usada essa funcao independente da implementacao
+void mostraMenu();
+
 int main(){
+    mostraMenu();
 
-    int vet[9];
-
-    for(int i = 0; i <=9; i++){
-        printf("\nInforme qual a posicao do codigo desejado:");
-        scanf("%d", vet[i]);
-    }
     return 0;
+}
+
+void mostraMenu(){
+    printf("### Menu de interacao ###\n");
+        printf("1 - Visualizar lista\n");
+        printf("2 - Consultar codigo\n");
+        printf("3 - Inserir codigo\n");
+        printf("4 - Remover codigo\n");
+        printf("5 - Visualizar quantidade de elementos\n");
+        printf("6 - Esvaziar lista\n");
+
 }
